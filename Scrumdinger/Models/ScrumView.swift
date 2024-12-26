acddc6e558fb6cebe25d81b3611c7130027569a3
2n0k460 <tunokabo@gmail.com>
2024-12-27 08:47:39 +0900

    セクション3-1

---
+++ Scrumdinger/Models/ScrumView.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct ScrumView: View {
+    let scrums: [DailyScrum]
+    var body: some View {
+        List(scrums, id: \.title) { scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColor)
+        }
+    }
+}
+
+struct ScrumView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumView(scrums: DailyScrum.sampleData)
+    }
+}
