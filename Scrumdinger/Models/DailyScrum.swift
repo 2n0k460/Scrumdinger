c774146a152f32f1e8de30062bbfbadeb3da8fd6
2n0k460 <tunokabo@gmail.com>
2024-12-27 12:11:58 +0900

    セクション4-1

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -1,26 +1,39 @@
 import Foundation
 
-struct DailyScrum {
+struct DailyScrum: Identifiable {
+    let id: UUID
     var title: String
     var attendees: [String]
     var lengthInMinutes: Int
     var theme: Theme
+    
+    init(id: UUID = UUID(), title: String, attendees: [String], lengthInMinutes: Int, theme: Theme) {
+        self.id = id
+        self.title = title
+        self.attendees = attendees
+        self.lengthInMinutes = lengthInMinutes
+        self.theme = theme
+    }
 }
 
 extension DailyScrum {
     static let sampleData: [DailyScrum] =
     [
-        DailyScrum(title: "Design",
-                   attendees: ["Cathy", "Daisy", "Simon", "Jonathan"],
-                   lengthInMinutes: 10,
+        DailyScrum(title: "Product Design",
+                   attendees: ["Sakaue", "Nogawa", "Kyou", "Kaku"],
+                   lengthInMinutes: 20,
+                   theme: .poppy),
+        DailyScrum(title: "Native Dev",
+                   attendees: ["Abel", "Narita", "Nicolas"],
+                   lengthInMinutes: 15,
                    theme: .yellow),
-        DailyScrum(title: "App Dev",
-                   attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"],
-                   lengthInMinutes: 5,
-                   theme: .orange),
-        DailyScrum(title: "Web Dev",
-                   attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"],
-                   lengthInMinutes: 5,
-                   theme: .poppy)
+        DailyScrum(title: "Backend Dev",
+                   attendees: ["Musha", "Brocken", "Kita", "Nagashima"],
+                   lengthInMinutes: 20,
+                   theme: .teal),
+        DailyScrum(title: "Story Owner",
+                   attendees: ["Asahara", "Tanigaki"],
+                   lengthInMinutes: 10,
+                   theme: .orange)
     ]
 }
