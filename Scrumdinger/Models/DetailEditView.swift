63163ebd6ab21c7fc31d565bf8906e58b350d4d8
2n0k460 <tunokabo@gmail.com>
2024-12-28 10:17:15 +0900

    セクション5-3

--- Scrumdinger/Models/DetailEditView.swift
+++ Scrumdinger/Models/DetailEditView.swift
@@ -12,6 +12,8 @@ struct DetailEditView: View {
                     Slider(value: $scrum.lengthInMinutesAsDouble, in: 5...30, step: 1) {
                         Text("Length")
                     }
+                    .accessibilityValue("\(scrum.lengthInMinutes) minutes")
+                        .accessibilityHidden(true)
                     Spacer()
                     Text("\(scrum.lengthInMinutes) minutes")
                 }
@@ -33,6 +35,7 @@ struct DetailEditView: View {
                         }
                     }) {
                         Image(systemName: "plus.circle.fill")
+                            .accessibilityLabel("Add attendee")
                     }
                     .disabled(newAttendeeName.isEmpty)
                 }
